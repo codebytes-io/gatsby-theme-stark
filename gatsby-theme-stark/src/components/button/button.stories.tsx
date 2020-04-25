0153182ea306af468fadbcacd7d39b297adfd56b
Tom <tom@codebytes.io>
2020-04-25 02:38:30 +0100

    add storybook

---
+++ gatsby-theme-stark/src/components/button/button.stories.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "./button";
+
+export default {
+  title: "ButtonNew",
+  component: Button,
+};
+
+export const Text = () => <Button>Hello Button</Button>;
